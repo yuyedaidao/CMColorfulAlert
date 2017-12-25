b36b0ca630bdf271fbf80948b52a4f64b48e832c
留白 <wyqpadding@gmail.com>
2017-12-25 10:38:23 +0800

    Initial Commit

---
+++ CMColorfulAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMColorfulAlert
+//
+//  Created by Wang on 2017/12/25.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CMColorfulAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CMColorfulAlert
+//
+//  Created by Wang on 2017/12/25.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
